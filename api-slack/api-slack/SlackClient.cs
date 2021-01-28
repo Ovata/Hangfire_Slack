44a5d0b1f26928a41678942a64e2ae4ad3de0fa1
ovata <ovataworld@gmail.com>
2021-01-28 16:47:12 +0100

    second commit

---
+++ api-slack/api-slack/SlackClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace api_slack
+{
+    public class SlackClient
+    {
+        private readonly Encoding _enconding = new UTF8Encoding();
+        public Uri _uri { get; }
+
+        public SlackClient(string urlWithAccessToken)
+        {
+            _uri = new Uri(urlWithAccessToken);
+        }
+
+        public void PostMessage(string text, string username = null, string channel = null)
+        {
+            Payload payload = new Payload()
+            {
+                Channel = channel,
+                Username = username,
+                Text = text
+            };
+
+            PostMessage(payload);
+        }
+
+        public void PostMessage(Payload payload)
+        {
+            string payloadJson = JsonConvert.SerializeObject(payload);
+
+            using (WebClient client = new WebClient())
+            {
+                NameValueCollection data = new NameValueCollection();
+                data["payload"] = payloadJson;
+
+                var response = client.UploadValues(_uri, "POST", data);
+
+                string responseText = _enconding.GetString(response);
+            }
+        }
+    }
+}
---
+++ api-slack/api-slack/SlackTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_slack
+{
+    public class SlackTest
+    {
+        void TestPostMessage()
+        {
+            string urlToken = "https://{your_account}.slack.com/services/hooks/incoming-webhook?token={https://hooks.slack.com/services/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}";
+
+
+            SlackClient client = new SlackClient(urlToken);
+
+            client.PostMessage(username: "kamleshbhor",
+                       text: "THIS IS A TEST MESSAGE!!",
+                       channel: "#general");
+        }
+    }
+}
